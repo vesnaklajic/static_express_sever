0ff67acba97d2807065669da9a99fa2e4fb437e0
vesnaklajic <vesnaklajic77@gmail.com>
2021-02-11 23:13:17 +0100

    Added data.json, tracking with visited

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # static_express_sever
+## Functional Reguirements:
+- Make an application for promotion Ski centres
+- Use picture and basic information 
+- use Static method for all Routes in Public file 
+- keep a track of the visits to every Ski centar Info 
+
+## Tecnical Reguirements:
+- organize Backend and Frontend file separately
+- use Static method for all Routes in Public file 
+- keep a track of the visits to every Ski centar  
+
+## Techno 
+-Node.js
+-Express.js
+- Fs.js
+
--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express();
+const data = require('./data');
+const fs = require('fs');
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/get_data/:id', (req, res) => {
+    console.log(data);
+    let id = req.params.id;
+    let oneSki = data.filter(el => {
+        return el.id == id;
+    })[0];
+    oneSki.visited++;
+    fs.writeFile(__dirname + "/data.json", JSON.stringify(data), (err) => {
+        if (err) throw err;
+        res.send(oneSki);
+    })
+    console.log(oneSki);
+
+})
 
 app.listen(3000, () => {
     console.log("Server running on 3000...");
